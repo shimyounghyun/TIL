def44a64ea0500245b64ff1f83a1d71db12f4f73
shimyounghyun <lhlrol1004@naver.com>
2020-04-05 23:36:11 +0900

    12851

--- algorithm/backjoon/12851.c
+++ algorithm/backjoon/12851.c
@@ -1,18 +1,3 @@
 #include <stdio.h>
 #include <stdlib.h>
-const int MAX=1e6;
-int n,m;
-int map[MAX+1];
 
-int main()
-{
-	int queue[MAX+1];
-	int front=0;
-	int rear=0;
-	int ans=0,cnt=0;
-	scanf("%d %d", &n, &m);
-	queue[rear++]=
-	while (!cnt){
-		int d = 
-	}
-}
---
+++ algorithm/backjoon/test.c
@@ -0,0 +1,8 @@
+#include <sys/queue.h>
+
+int main(){
+
+	queue<int> q;
+
+	q.push(1);
+}
