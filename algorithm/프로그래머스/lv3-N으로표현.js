eaa9a29c8bd57dade50f3e77af5beecde29cc52d
shimyounghyun <lhlrol1004@naver.com>
2020-05-14 17:27:47 +0900

    N으로표현

--- algorithm/프로그래머스/lv3-N으로표현.js
+++ algorithm/프로그래머스/lv3-N으로표현.js
@@ -1,1 +1,33 @@
-int min; public void cal(int N, int number, int count, long result) { if(min<count) return; if(result==number) { if(count<min) min=count; return; } if(count==8) return; else { int rest = 8-count; for(int i=1;i<=rest;i++) { int tailmax = i/2; for(int j=0;j<=tailmax;j++) { int head = 0; for(int k=1;k<=i-j;k++) { head = head*10 + N; } int next = head; int tail = 0; for(int k=1;k<=j;k++) { tail = tail*10 + N; } if(tail>0) next/=tail; cal(N,number,count+i,result+next); cal(N,number,count+i,result-next); cal(N,number,count+i,result*next); cal(N,number,count+i,result/next); cal(N,number,count+i,result*-1*next); cal(N,number,count+i,result*-1/next); } if(i>2) { cal(N,number,count+i,result); cal(N,number,count+i,0); } } } } public int solution(int N, int number) { min = 9; cal(N, number, 0, 0); if(min>8) min = -1; return min; }
+var dp = {};
+function solution(N, number) {
+	var count = 1;
+    
+    dp[count++] = [N];
+    if (N == number)
+        return 1;
+    while (count <= 8){
+        if (dp[count-1][0] * 10 + N == number)
+            return count;
+        dp[count] = [dp[count-1][0] * 10 + N];
+        for (var i=1; i<count; i++){
+            for (var j=0; j<dp[i].length; j++){
+            	var n = dp[i][j];
+            	for (var k=0; k<dp[count-i].length; k++){
+            		var m = dp[count-i][k];
+                    if (n+m == number || n-m == number || n*m == number)
+                        return count;
+                    dp[count].push(n+m);
+                    dp[count].push(n-m);
+                    dp[count].push(n*m);
+                    if (m){
+                        if (parseInt(n/m) == number)
+                            return count;
+                    	dp[count].push(parseInt(n/m));
+                    }
+                }    
+            }
+        }
+        count++;
+    }
+    return -1;
+}
