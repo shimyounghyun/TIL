4e84dab01357e6acb010368d0035ee7f6d388b8c
Younghyun Shim <lhlrol1004@naver.com>
2020-07-09 12:18:42 +0900

    lv4도둑질

---
+++ algorithm/프로그래머스/lv4-도둑질.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] money) {
+       int[][] dp = new int[2][money.length];
+        dp[0][0] = money[0];
+        dp[0][1] = money[0];
+        dp[1][0] = 0;
+        dp[1][1] = money[1];
+        
+        for (int i=2; i<money.length; i++){
+            dp[0][i] = Math.max(dp[0][i-2]+money[i], dp[0][i-1]);
+            dp[1][i] = Math.max(dp[1][i-2]+money[i], dp[1][i-1]);
+        }
+        return Math.max(dp[0][money.length-2], dp[1][money.length-1]);
+    }
+}
