2c4f9fb8a893d2a840b946950ab5a1ad5912e3d0
shimyounghyun <lhlrol1004@naver.com>
2020-07-11 10:31:13 +0900

    파일 정리

--- algorithm/프로그래머스/lv2-멀쩡한 사각형.js
+++
@@ -1,7 +0,0 @@
-function solution(w,h){
-    let box = 0;
-    for(let i = 0; i < w; i++){
-        box += h - (Math.ceil((h / w).toFixed(5) * (i + 1)) - Math.floor((h / w).toFixed(5) * i))
-    }
-	return box;
-}
