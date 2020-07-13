f2ee694c0f7473bd4b0415a3a93882561362e732
shimyounghyun <lhlrol1004@naver.com>
2020-07-13 16:41:11 +0900

    update two pointer

--- algorithm/leetcode/longest-substring-without-repeating-characters.java
+++ algorithm/leetcode/longest-substring-without-repeating-characters.java
@@ -1,16 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         int answer = 0;
+        int left = 0;
+        int right = 0;
         
-        for (int i=0; i<s.length(); i++){
-            String c = "";   
-            for (int j=i; j<s.length(); j++){
-                if (c.contains(s.charAt(j)+"")){
-                    break;
-                }
-                c += s.charAt(j);
+        Set<Character> list = new HashSet<>();
+        while (right < s.length()){         
+            if (list.contains(s.charAt(right))){                
+                list.remove(s.charAt(left));
+                left++;
+            }else{
+                list.add(s.charAt(right));
+                right++;                
+                answer = answer < list.size() ? list.size() : answer;
             }
-            answer = answer < c.length() ? c.length() : answer;
         }
         
         return answer;
