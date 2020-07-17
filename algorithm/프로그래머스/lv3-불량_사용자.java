3423fce67754bffe9b2490aedb638b3ab18607b4
shimyounghyun <lhlrol1004@naver.com>
2020-07-17 18:16:13 +0900

    lv3불량사용자

--- algorithm/프로그래머스/lv3-불량_사용자.java
+++ algorithm/프로그래머스/lv3-불량_사용자.java
@@ -1,46 +1,22 @@
 import java.util.*;
 
 class Solution {
-    Set<String> list = new HashSet<>();
-    
-    public int dfs(String[] user_id, String[] banned_id, int index){
-        int answer = 0;
-        if (index == banned_id.length){
-            int result = 0;
-            for (int i=0; i<banned_id.length; i++){
-                if (banned_id[i].length() != user_id[i].length())
-                    return 0;
-                int j = 0;
-                while (j < user_id[i].length()
-                      && (banned_id[i].charAt(j) == '*' 
-                          || user_id[i].charAt(j) == banned_id[i].charAt(j)))
-                    j++;
-                if (j == user_id[i].length())
-                    result++;
-            }
-            if (result == banned_id.length){
-                List<String> s = new ArrayList<>();
-                for (int i=0; i<banned_id.length; i++){
-                    s.add(user_id[i]);
-                }
-                Collections.sort(s);
-                if (list.add(String.join("",s)))
-                    return 1;
-            }
-            return 0;
-        }
-        for (int i=index; i<user_id.length; i++){
-            String temp = user_id[index];
-            user_id[index] = user_id[i];
-            user_id[i] = temp;
-            answer += dfs(user_id, banned_id, index+1);
-            user_id[i] = user_id[index];
-            user_id[index] = temp;
-        }
-        return answer;
-    }
+    Set<Integer> set = new HashSet<>();
     
     public int solution(String[] user_id, String[] banned_id) {
-        return dfs(user_id, banned_id, 0);
+        dfs(user_id, banned_id, 0, 0);
+        return set.size();
+    }
+    
+    public void dfs(String[] user_id, String[] banned_id, int index, int bit){
+        if (index == banned_id.length){
+            set.add(bit);
+            return;
+        }
+        String reg = banned_id[index].replace("*","[\\w\\d]");
+        for (int i=0; i<user_id.length; i++){
+            if ((bit>>i & 1) == 1 || !user_id[i].matches(reg)) continue;
+            dfs(user_id, banned_id, index+1, (bit | (1<<i)));
+        }
     }
 }
