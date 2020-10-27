be7de450262c6d1712062d0e285c8ac7884f3577
shimyounghyun <lhlrol1004@naver.com>
2020-10-27 17:19:52 +0900

    lv3가장먼노드

--- algorithm/프로그래머스/lv3-가장_먼_노드.java
+++ algorithm/프로그래머스/lv3-가장_먼_노드.java
@@ -2,44 +2,54 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        int answer = 0;
-        int count = 1;
-        boolean[][] map = new boolean[n+1][n+1];
+        int[] dist = new int[n+1];
         boolean[] visit = new boolean[n+1];
-        
+        int answer = 0;
+        List<Integer>[] list = new List[n+1];
+        for (int i=1; i<=n; i++)
+            list[i] = new LinkedList<>();
         for (int i=0; i<edge.length; i++){
-            int from = edge[i][0];
-            int to = edge[i][1];
-            map[from][to] = true;
-            map[to][from] = true;
+            list[edge[i][0]].add(edge[i][1]);
+            list[edge[i][1]].add(edge[i][0]);
         }
+        
+        PriorityQueue<Node> q = new PriorityQueue<>();
         visit[1] = true;
-        LinkedList<Integer> list = new LinkedList<Integer>();
-        LinkedList<Integer> wait = new LinkedList<Integer>();
-        for (int i=1; i<n+1; i++){
-            if (map[1][i]){
-                list.add(i);
-                visit[i] = true;
-                count++;
+        for(int i=0; i<list[1].size(); i++){
+            q.add(new Node(list[1].get(i), 1));
+            dist[list[1].get(i)] = 1;
+        }
+        int max = 0;
+        while (!q.isEmpty()){
+            Node node = q.poll();
+            visit[node.num] = true;
+            
+            for(int i=0; i<list[node.num].size(); i++){
+                int num = list[node.num].get(i);
+                if (visit[num] || dist[num] != 0) continue;
+                dist[num] = node.val +1;
+                int count = dist[num];
+                max = Math.max(max, count);
+                q.add(new Node(num, count));
             }
         }
-        while (count < n){
-            answer = 0;
-            while (!list.isEmpty()){
-                int k = list.poll();
-                for (int i=1; i<n+1; i++){
-                    if (visit[i] == false && map[k][i]){
-                        wait.add(i);
-                        count++;
-                        answer++;
-                        visit[i] = true;
-                    }
-                }                
-            }            
-            while (!wait.isEmpty()){
-                list.add(wait.poll());
-            }
-        }
+        
+        for (int i=1; i<dist.length; i++)
+            if (max == dist[i]) answer++;
         return answer;
+    }
+    
+    public class Node implements Comparable<Node> {
+        int num;
+        int val;
+        
+        public Node(int num, int val){
+            this.num = num;
+            this.val = val;
+        }
+        
+        public int compareTo(Node n){
+            return Integer.compare(this.val, n.val);
+        }
     }
 }
