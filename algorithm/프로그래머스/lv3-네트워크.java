de48fdba00dd2b7c2df6c9b02565e2fc95a4e094
shimyounghyun <lhlrol1004@naver.com>
2020-10-28 17:14:37 +0900

    lv3네트워크

--- algorithm/프로그래머스/lv3-네트워크.java
+++ algorithm/프로그래머스/lv3-네트워크.java
@@ -1,47 +1,26 @@
 import java.util.*;
 
 class Solution {
+    boolean[] visit;
     
-    public void dfs (boolean[][] network, int[][] computers, int r, int c){
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        network[r][c] = true;        
-        for (int i=0; i<computers[c].length; i++){
-            if (i == c)
-                continue;
-            if (computers[c][i] == 1 && network[c][i] == false){
-                list.add(i);
-                network[c][i] = true;
-                network[i][c] = true;
+    public void dfs(int target, int size, int[][] computers){
+        for (int i=0; i<size; i++){
+            if (computers[target][i] == 1 && visit[i] == false){
+                visit[i] = true;
+                dfs(i, size, computers);
             }
-        }
-        int m = 0;
-        while (m < list.size()){
-            int n = list.get(m);
-            for(int i=0; i<computers[n].length; i++){
-                if (computers[n][i] == 1 && network[n][i] == false){
-                    list.add(i);
-                    network[n][i] = true;
-                    network[i][n] = true;
-                }
-            }
-            m++;
         }
     }
     
     public int solution(int n, int[][] computers) {
-        boolean[][] network = new boolean[n][n];
-        
-        int count = 0;
+        int answer = 0;
+        visit = new boolean[n];
         for (int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
-                if (network[i][j])
-                    continue;
-                if (computers[i][j] == 1 && network[i][j] == false){
-                    dfs(network, computers, i, j);
-                    count++;
-                }
-            }
+            if (visit[i]) continue;
+            dfs(i, n, computers);
+            answer++;
         }
-        return count;
+        
+        return answer;
     }
 }
