e67274ef4269301b40939350f5d8f052c10e477d
shimyounghyun <lhlrol1004@naver.com>
2020-12-22 15:02:58 +0900

    update 3sum

--- algorithm/leetcode/3sum.java
+++ algorithm/leetcode/3sum.java
@@ -1,31 +1,30 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i=0; i<nums.length-1; i++){
-            if (i == 0 || nums[i-1] < nums[i]){
-                int j = i+1;
-                int k = nums.length - 1;
-                while (j < k){
-                    int sum = nums[i]+nums[j]+nums[k];
-                    if (sum == 0){
-                        List<Integer> list = new ArrayList<Integer>();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        result.add(list);
-                        while (j < k && nums[k] == nums[k-1])
-                            k--;
-                        while (j < k && nums[j] == nums[j+1])
-                            j++;
-                    }else if(sum > 0){
-                        k--;
-                    }else{
-                        j++;
-                    }
+        for (int i=0; i<nums.length; i++){
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = nums.length-1;
+            while (j < k){
+                if (nums[i]+nums[j]+nums[k] == 0){
+                    List<Integer> sum = new ArrayList<>();
+                    sum.add(nums[i]);
+                    sum.add(nums[j]);
+                    sum.add(nums[k]);
+                    list.add(sum);
+                    while (j+1<k && nums[j] == nums[j+1]) j++;
+                    while (j<k-1 && nums[k] == nums[k-1]) k--;
+                    j++;
+                }else if (nums[i]+nums[j]+nums[k] < 0){
+                    j++;
+                }else{
+                    k--;
                 }
             }
         }
-        return (result);
+        return list;
     }
 }
