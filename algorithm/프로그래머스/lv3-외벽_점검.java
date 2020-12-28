3641e4f5e50573817b9669742c22564c450f5dd4
shimyounghyun <lhlrol1004@naver.com>
2020-12-28 11:35:27 +0900

    외벽점검 수정

--- algorithm/프로그래머스/lv3-외벽_점검.java
+++ algorithm/프로그래머스/lv3-외벽_점검.java
@@ -2,59 +2,43 @@ import java.util.*;
 
 class Solution {
     int answer = -1;
-    public void permutation(int[] c, int start, int end,
-                           int index, int[] d){
-        if (index == d.length){
-            // for (int t : d){
-            //     System.out.print(t + " ");
-            // }
-            // System.out.println("");
-            int count=0;
+    public void dfs(int[] circle, int index, int[] arr){
+        if (index == arr.length){
             int current = -1;
-            int k=0;
-            for (int j=start; j<end; j++){
-                if (current >= c[j])
-                    continue;
-                if (k >= d.length){
-                    count = -1;
-                    break;
-                }
-                current = c[j]+d[d.length-k-1];
+            int count = 0;
+            int arr_index = 0;
+            for (int i=0; i<circle.length; i++){
+                if (current >= circle[i]) continue;
+                if (arr_index >= arr.length) break;
+                current = circle[i]+arr[arr_index];
+                arr_index++;
                 count++;
-                k++;
             }
-            if (answer == -1){
-                answer = count;
-            }else if (count != -1){
-                answer = Math.min(answer, count);
+            if (current >= circle[circle.length-1]){
+                answer = answer==-1 ? count : Math.min(answer, count);
             }
             return;
         }
-        for (int i=index; i<d.length; i++){
-            int temp = d[i];
-            d[i]=d[index];
-            d[index]=temp;
-            permutation(c, start, end, index+1, d);
-            d[index]=d[i];
-            d[i]=temp;
+        for (int i=index; i<arr.length; i++){
+            int temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+            dfs(circle, index+1, arr);
+            arr[index] = arr[i];
+            arr[i] = temp;
         }
     }
     
     public int solution(int n, int[] w, int[] d) {
-        if (w.length == 1)
-            return 1;
-        int[] c = new int[w.length*2-1];
+        
+        int[] range = new int[w.length*2-1];
         for (int i=0; i<w.length; i++){
-            c[i]=w[i];
-            if (i < w.length-1)
-                c[w.length+i]=w[i]+n;
+            range[i] = w[i];
+            if (i < w.length-1) range[i+w.length] = range[i]+n;
         }
-        for (int t : c){
-            System.out.print(t+" ");
-        }
-        // System.out.println("");
+        
         for (int i=0; i<w.length; i++){
-            permutation(c,i,i+w.length,0,d);
+            dfs(Arrays.copyOfRange(range, i, i+w.length), 0, d);
         }
         return answer;
     }
